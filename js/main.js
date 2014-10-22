6fa118eb500ad4ed4336aec838f663081412c804
Diego Moreira <diego.moreira@inspira.com.br>
2014-10-22 19:32:55 -0200

    Start carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+var common = (function () {
+  'use strict';
 
+  var   carousel_width = 300,
+        carousel_elem = $(".carousel .item").length,
+        carousel_size = carousel_elem * carousel_width,
+        limit_position = carousel_size - carousel_width;
+
+  return {
+    init: function () {
+      common.set_carousel_size();
+      // common.act_left();
+    },
+
+    set_carousel_size: function(){
+        $(".carousel").width(carousel_size);
+    },
+
+    // act_left: function(){
+    //     var position = $(".carousel").position().left;
+    //         limit_position = carousel_size - carousel_width;
+        
+    //     while((position * -1) < limit_position){
+    //         position = (-1 * (position + 300));
+    //         $(".carousel").css("left", "'" + position + "'");
+    //     }
+    // }
+
+  };
+
+})();
+
+$(document).ready(common.init);
