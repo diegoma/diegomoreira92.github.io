b209a5ca3631c2cfba89c58af4008b00da7922dc
Diego Moreira <diego.moreira@inspira.com.br>
2014-11-17 18:28:21 -0200

    trying postgresql and create a route

--- site/routes/index.js
+++ site/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/teste', function(req, res) {
+  res.render('teste', { title: 'teste teste' });
+});
+
 module.exports = router;
