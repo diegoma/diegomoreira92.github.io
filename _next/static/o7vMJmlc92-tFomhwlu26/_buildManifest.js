473dd83a8147df9798c20f4816a8f4aa7c480b8f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-04 19:55:34 +0000

    Deploy diegoma/diegoma.github.io to github.com/diegoma/diegoma.github.io.git:gh-pages

---
+++ _next/static/o7vMJmlc92-tFomhwlu26/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-cdd8f2d4c3525bb4be2a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-21de763c01ff9227b21c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
