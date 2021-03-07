13af9f88430598aa3292ac02a2a409c594fb60ad
Diego Moreira Alves <diegomoreiraalves.92@gmail.com>
2021-03-06 23:17:22 -0300

    [0004] Marcação de Seções e adição de componentes
    - Marcação de Seções e adição de componentes
    -- Adição da marcação de onde os componentes da página devem ser
    adicionados.
    -- Adição dos componentes:
    --- QuemSouEu

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,41 @@
 import styled from 'styled-components'
 import { Grid, Cell } from "styled-css-grid";
+import db from '../db.json';
 import SiteBackground from '../src/components/SiteBackground';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Menu from '../src/components/Menu'
+import QuemSouEu from '../src/components/QuemSouEu'
 import Footer from '../src/components/Footer'
 import Separador from '../src/components/Separador';
 
-const Title = styled.h1`
-  font-size: 30px;
-  color: ${({ theme }) => theme.colors.primary};
-  text-align: center;
-  text-transform: uppercase;
-  font-family: Arial, Helvetica, sans-serif;
-`
 export default function Home() {
   return (
     <SiteBackground>
       <GitHubCorner projectUrl="https://github.com/diegoma/diegoma.github.io" />
 
       <Grid style={{ width: '100%' }}>
-        <Cell left={2} width={10}> 
-          <Menu></Menu>
+        <Cell left={2} width={10}>
+          <Menu menuItems={db.menuItems}></Menu>
         </Cell>
 
-        <Cell width={12}> 
+        <Cell left={3} width={8}>
+          <QuemSouEu quemSouEu={db.quemSouEu} redesSociais={db.redesSociais}></QuemSouEu>
+          <Separador></Separador>
+        </Cell>
+
+        <Cell left={2} width={10}>
+          {/* Experiência */}
+        </Cell>
+
+        <Cell left={2} width={10}>
+          {/* Educação */}
+        </Cell>
+
+        <Cell left={2} width={10}>
+          {/* Carreira */}
+        </Cell>
+
+        <Cell width={12}>
           <Separador></Separador>
           <Footer></Footer>
         </Cell>
