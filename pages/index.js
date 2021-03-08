8283438b850605a284438ece30901f75d7285f37
Diego Moreira Alves <diegomoreiraalves.92@gmail.com>
2021-03-08 02:58:33 -0300

    [0004] Adição do componente Educação e hack para ancoras de navegação

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ import GitHubCorner from '../src/components/GitHubCorner';
 import Menu from '../src/components/Menu';
 import QuemSouEu from '../src/components/QuemSouEu';
 import Experiencia from '../src/components/Experiencia';
+import Educacao from '../src/components/Educacao';
 import Footer from '../src/components/Footer';
 import Separador from '../src/components/Separador';
 
 export default function Home() {
   return (
     <SiteBackground>
+      <span id="quemSouEu"></span>
       <GitHubCorner projectUrl="https://github.com/diegoma/diegoma.github.io" />
 
       <Grid style={{ width: '100%' }}>
@@ -19,7 +21,7 @@ export default function Home() {
           <Menu menuItems={db.menuItems}></Menu>
         </Cell>
 
-        <Cell left={3} width={8}>
+        <Cell left={3} width={8}>        
           <QuemSouEu quemSouEu={db.quemSouEu} redesSociais={db.redesSociais}></QuemSouEu>
         </Cell>
 
@@ -28,11 +30,13 @@ export default function Home() {
         </Cell>
 
         <Cell left={2} width={10}>
+          <span id="experiencia"></span>
           <Experiencia experiencia={db.experiencia} habilidades={db.habilidades}></Experiencia>
         </Cell>
 
         <Cell left={2} width={10}>
-          {/* Educação */}
+          <span id="educacao"></span>
+          <Educacao educacao={db.educacao}></Educacao>
         </Cell>
 
         <Cell left={2} width={10}>
